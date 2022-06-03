1fc872e951fdefe4f2ae5c4e12dd1df9cef3e00d
jingkai27 <jingkai.t27@gmail.com>
2022-06-03 12:38:43 +0800

    day 15 edits
    - cleaned up the contact-me page
    - added resume
    - tidied up all the links
    - added meta tag for resizing issues
    - included javascript for sending contact me page
    - found a free place to send forms out for free

---
+++ main.js
@@ -0,0 +1,32 @@
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thank you! I will get back to you once I see your message. Have a nice day!";
+      status.classList.add('success')
+      form.reset()
+    } else {
+      status.classList.add("error")
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
